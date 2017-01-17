b2c2efc34f838aa6463df1baa8de51274a892d89
incisivebox <maroni720@gmail.com>
2017-01-17 11:57:02 -0600

    test commit
    test commit pls ignore

---
+++ desktop/src/com/mygdx/overstory/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.overstory.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.overstory.OverstoryMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OverstoryMain(), config);
+	}
+}

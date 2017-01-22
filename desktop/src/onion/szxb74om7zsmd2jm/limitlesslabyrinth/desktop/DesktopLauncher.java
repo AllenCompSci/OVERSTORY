c42fded57d28170b3635067480210574783f98b0
incisivebox <maroni720@gmail.com>
2017-01-21 21:37:40 -0600

    added fresh project file
    game is called limitless labyrinth. set up package name using a .onion
    domain

---
+++ desktop/src/onion/szxb74om7zsmd2jm/limitlesslabyrinth/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package onion.szxb74om7zsmd2jm.limitlesslabyrinth.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import onion.szxb74om7zsmd2jm.limitlesslabyrinth.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}

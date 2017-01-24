be705a8cf0caa612913d47bda6004facb5c9c623
DLGesus <chrisdelagarza@verizon.net>
2017-01-23 18:45:44 -0600

    Latest Changes

--- core/src/onion/szxb74om7zsmd2jm/limitlesslabyrinth/entities/Entity.java
+++ core/src/onion/szxb74om7zsmd2jm/limitlesslabyrinth/entities/Entity.java
@@ -6,12 +6,14 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import onion.szxb74om7zsmd2jm.limitlesslabyrinth.mechanics.Detection;
 
 /**
  * Created by chris on 1/22/2017.
  */
-public class Entity implements InputProcessor {
+public class Entity extends Actor implements InputProcessor {
     public Sprite getSprite() {
         return sprite;
     }
@@ -25,6 +27,10 @@ public class Entity implements InputProcessor {
         return health;
     }
 
+    public void setHealth(float health) {
+        this.health = health;
+    }
+
     protected float health;
 
 
@@ -35,6 +41,8 @@ public class Entity implements InputProcessor {
         this.collisionLayer = collisionLayer;
         this.health = health;
         sprite.setPosition(32 * x, 32 * y);
+        setBounds(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
+        setTouchable(Touchable.enabled);
     }
 
 

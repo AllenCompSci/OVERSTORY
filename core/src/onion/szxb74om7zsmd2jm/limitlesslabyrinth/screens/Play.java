e2f4e1e16045acd96f6f3692222ea7bc6f6a65be
DLGesus <chrisdelagarza@verizon.net>
2017-01-24 00:00:05 -0600

    Added SpawnTile

--- core/src/onion/szxb74om7zsmd2jm/limitlesslabyrinth/screens/Play.java
+++ core/src/onion/szxb74om7zsmd2jm/limitlesslabyrinth/screens/Play.java
@@ -12,16 +12,23 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import onion.szxb74om7zsmd2jm.limitlesslabyrinth.entities.Enemy;
 import onion.szxb74om7zsmd2jm.limitlesslabyrinth.entities.Player;
+import onion.szxb74om7zsmd2jm.limitlesslabyrinth.threads.Spawn;
+
+import java.util.Random;
 
 /**
  * Created by chris on 1/19/2017.
  */
 public class Play implements Screen {
 
-    private TiledMap map;
+    public TiledMap getMap() {
+        return map;
+    }
+    private static TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
     public Player getPlayer() {
@@ -30,6 +37,8 @@ public class Play implements Screen {
     private static Player player;
     private Array<Enemy> enemies = new Array<Enemy>();
     private InputMultiplexer im;
+    private int[][] spawnTiles;
+    private long time = 0;
 
     @Override
     public void show() {
@@ -38,7 +47,7 @@ public class Play implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map);
 
         camera = new OrthographicCamera();
-        camera.zoom = .6f;
+        camera.zoom = 4f;
         camera.setToOrtho(false);
 
         player = new Player(new Sprite(new Texture("thor32.png")), 5, 5, 100f, (TiledMapTileLayer) map.getLayers().get(1));
@@ -50,6 +59,7 @@ public class Play implements Screen {
         spawnEnemy(new Sprite(new Texture("still1.png")), 16, 16, 100f,  (TiledMapTileLayer) map.getLayers().get(1));
         spawnEnemy(new Sprite(new Texture("still1.png")), 17, 17, 100f,  (TiledMapTileLayer) map.getLayers().get(1));
         spawnEnemy(new Sprite(new Texture("still1.png")), 18, 18, 100f,  (TiledMapTileLayer) map.getLayers().get(1));
+        spawnTiles = (checkMapLayerFor((TiledMapTileLayer) map.getLayers().get(1), "spawnEnemy"));
     }
 
     @Override
@@ -69,6 +79,15 @@ public class Play implements Screen {
         }
         player.draw(renderer.getBatch());
         renderer.getBatch().end();
+
+        //Spawning in enemies every n seconds
+        Random rand = new Random();
+        int num = 0;
+        num = rand.nextInt(spawnTiles.length);
+        if (System.currentTimeMillis() > time) {
+            spawnEnemy(new Sprite(new Texture("still1.png")), spawnTiles[num][0], spawnTiles[num][1], 100f, (TiledMapTileLayer) getMap().getLayers().get(1));
+            time = System.currentTimeMillis() + 1000;
+        }
     }
 
     @Override
@@ -97,10 +116,32 @@ public class Play implements Screen {
     public void dispose() {
         map.dispose();
         renderer.dispose();
+
     }
 
     public void spawnEnemy(Sprite sprite, float x, float y, float health, TiledMapTileLayer collisionLayer){
         enemies.add(new Enemy(sprite, x, y, health, collisionLayer));
         im.addProcessor(enemies.get(enemies.size - 1));
+    }
+
+    public int[][] checkMapLayerFor(TiledMapTileLayer layer, String string){
+        int count = 0;
+        for(int x = 0; x < layer.getWidth(); x++){
+            for(int y = 0; y < layer.getHeight(); y++){
+                if(layer.getCell(x, y).getTile().getProperties().containsKey(string)) count++;
+            }
+        }
+        int[][] tiles = new int[count][2];
+        Gdx.app.log(String.valueOf(layer.getHeight()), string.valueOf(layer.getWidth()));
+        count = 0;
+        for(int x = 0; x < layer.getWidth(); x++){
+            for(int y = 0; y < layer.getHeight(); y++){
+                if(layer.getCell(x, y).getTile().getProperties().containsKey(string)) {
+                    tiles[count][0] = x;
+                    tiles[count++][1] = y;
+                }
+            }
+        }
+        return tiles;
     }
 }

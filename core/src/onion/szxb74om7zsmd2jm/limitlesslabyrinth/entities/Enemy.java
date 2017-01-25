0a8de9bc1bc738ca4e2f622c21c765f2c49f1089
RyanK <ryan.r.king@student.allenisd.org>
2017-01-25 14:46:25 -0600

    Added Enemy tracking
    Enemies follow player and fixed detection circle

--- core/src/onion/szxb74om7zsmd2jm/limitlesslabyrinth/entities/Enemy.java
+++ core/src/onion/szxb74om7zsmd2jm/limitlesslabyrinth/entities/Enemy.java
@@ -7,12 +7,14 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import onion.szxb74om7zsmd2jm.limitlesslabyrinth.mechanics.Detection;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * Created by chris on 1/21/2017.
  */
 public class Enemy extends Entity{
+    private ArrayList<Integer> canmove = new ArrayList<Integer>(); //Specifies which directions an enemy can move
 
     public Enemy(Sprite sprite, float x, float y, float health, TiledMapTileLayer collisionLayer) {
         super(sprite, x, y, health, collisionLayer);
@@ -33,28 +35,55 @@ public class Enemy extends Entity{
 
     @Override
     public void move() {
+        //Checks what side of player enemy is on
+        //Above
+        if (sprite.getY() + sprite.getHeight() / 2 > pl.getPlayer().getSprite().getY() + pl.getPlayer().getSprite().getHeight() / 2) {
+            canmove.add(3);
+        }
+        //Below
+        if (sprite.getY() + sprite.getHeight() / 2 < pl.getPlayer().getSprite().getY() + pl.getPlayer().getSprite().getHeight() / 2) {
+            canmove.add(4);
+        }
+        //Left
+        if(sprite.getX() + sprite.getWidth()/2 > pl.getPlayer().getSprite().getX() + pl.getPlayer().getSprite().getWidth()/2){
+            canmove.add(2);
+        }
+        //Right
+        if(sprite.getX() + sprite.getWidth()/2 < pl.getPlayer().getSprite().getX() + pl.getPlayer().getSprite().getWidth()/2){
+            canmove.add(1);
+        }
+        int num;
         Random rand = new Random();
-        int num = rand.nextInt(5 + 1);
-        if(num == 1){
-            if (checkCollision(0f, sprite.getHeight(), 0f, speed)) {
-                sprite.setY(sprite.getY() + speed);
+            if(canmove.size() == 0){
+                num = rand.nextInt(5 + 1);
+            }
+            else{
+                num = canmove.get(rand.nextInt(canmove.size()));
+            }
+
+        if(!detection.isInSmallRadius(this)) {
+            if (num == 4) { //Up
+                if (checkCollision(0f, sprite.getHeight(), 0f, speed)) {
+                    sprite.setY(sprite.getY() + speed);
+                }
+            }
+            if (num == 3) { //Down
+                if (checkCollision(0f, -sprite.getHeight(), 0f, -speed)) {
+                    sprite.setY(sprite.getY() + -speed);
+                }
+            }
+            if (num == 2) { //Left
+                if (checkCollision(-sprite.getWidth(), 0f, -speed, 0f)) {
+                    sprite.setX(sprite.getX() + -speed);
+                }
+            }
+            if (num == 1) { // Right
+                if (checkCollision(sprite.getWidth(), 0f, speed, 0f)) {
+                    sprite.setX(sprite.getX() + speed);
+                }
             }
         }
-        if(num == 2){
-            if (checkCollision(0f, -sprite.getHeight(), 0f, -speed)) {
-                sprite.setY(sprite.getY() + -speed);
-            }
-        }
-        if(num == 3){
-            if (checkCollision(-sprite.getWidth(), 0f, -speed, 0f)) {
-                sprite.setX(sprite.getX() + -speed);
-            }
-        }
-        if(num == 4){
-            if (checkCollision(sprite.getWidth(), 0f, speed, 0f)) {
-                sprite.setX(sprite.getX() + speed);
-            }
-        }
+        canmove.clear();
     }
 
 }
